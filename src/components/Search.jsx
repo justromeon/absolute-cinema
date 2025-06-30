6f0db06275cf87ef1c623835f2628f2e6f6d5cf7
Justin Kian Romeon <romeonjustin@gmail.com>
2025-06-30 16:03:48 +0800

    refactor: fix indentation of Search and Spinner components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
-    return (
-        <div className="search">
-            <div>
-                <img src="search.svg" alt="search" />
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
 
-                <input
-                    type="text"
-                    placeholder="Search through thousands of movies..."
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-
-                />
-            </div>
-        </div>
-    )
+        <input 
+          type="text"
+          placeholder="Search thousands shows available..."
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Search
