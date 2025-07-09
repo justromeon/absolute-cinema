f54c203415e9df06f0e063aefe1135282ffc1de8
Justin Kian Romeon <romeonjustin@gmail.com>
2025-07-09 23:26:03 +0800

    style: correct indentation of trycatch block in movieSevice

--- src/movieService.js
+++ src/movieService.js
@@ -10,25 +10,25 @@ export const updateMovieSeachCount = async (movie) => {
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
     : '';
 
-    try {
-      const response = await fetch('/api/searchCount', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify( {movie_id: movie.id, poster_url: posterUrl} )
-      });
+  try {
+    const response = await fetch('/api/searchCount', {
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      body: JSON.stringify( {movie_id: movie.id, poster_url: posterUrl} )
+    });
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'Failed to update movie search count');
-      }
-      return await response.json();
-
-    } catch (error) {
-      console.log(`[movieService (updateMovieSeachCount)] Error updating movie search count for movie_id ${movie.id}:`, error);
-      throw error; // Re-throw error for App.jsx to handle it
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Failed to update movie search count');
     }
+    return await response.json();
+
+  } catch (error) {
+    console.log(`[movieService (updateMovieSeachCount)] Error updating movie search count for movie_id ${movie.id}:`, error);
+    throw error; // Re-throw error for App.jsx to handle it
+  }
 
 }
 
